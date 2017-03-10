deba83160124ed0686cb69c31ae566558e95518e
Steve Geinosky <stephengeinosky@gmail.com>
2017-03-10 13:57:29 -0700

    Add continue-on-failure to not exit if a site update fails. Updated the readme to include force-git-mode and continue-on-failure commands.

--- src/Commands/ApplyCommand.php
+++ src/Commands/ApplyCommand.php
@@ -3,6 +3,8 @@
 namespace Pantheon\TerminusMassUpdate\Commands;
 
 // @TODO: Autoloading.
+use Pantheon\Terminus\Exceptions\TerminusException;
+
 require_once "MassUpdateCommandBase.php";
 
 class ApplyCommand extends MassUpdateCommandBase
@@ -25,8 +27,10 @@ class ApplyCommand extends MassUpdateCommandBase
      * @option boolean $updatedb Run update.php after updating (Drupal only)
      * @option boolean $accept-upstream Attempt to automatically resolve conflicts in favor of the upstream
      * @option dry-run Don't actually apply the updates
+     * @option force-git-mode Force Git mode on the environment
+     * @option continue-on-failure Don't exit if a site update fails
      */
-    public function applyAllUpdates($options = ['upstream' => '', 'updatedb' => false, 'accept-upstream' => false, 'dry-run' => false, 'force-git-mode' => false])
+    public function applyAllUpdates($options = ['upstream' => '', 'updatedb' => false, 'accept-upstream' => false, 'dry-run' => false, 'force-git-mode' => false, 'continue-on-failure' => false])
     {
         $site_updates = $this->getAllSitesAndUpdates($options);
         foreach ($site_updates as $info) {
@@ -50,6 +54,7 @@ class ApplyCommand extends MassUpdateCommandBase
                         }
                         $this->log()->notice($workflow->getMessage());
                     }
+                    $this->applyUpdates($options, $site, $updates, $env);
                 }
                 else {
                     $this->log()->warning(
@@ -59,22 +64,43 @@ class ApplyCommand extends MassUpdateCommandBase
                 }
             }
             else {
-                $logname = $options['dry-run'] ? 'DRY RUN' : 'notice';
-                $this->log()->notice(
-                    'Applying {updates} updates to {site}',
-                    ['site' => $site->getName(), 'updates' => count($updates), 'name' => $logname]);
+                $this->applyUpdates($options, $site, $updates, $env);
+            }
+        }
+    }
 
-                // Do the actual updates if we're not in dry-run mode
-                if (!$options['dry-run']) {
-                    // @TODO: We may be able to run workflows asynchronously to save time.
-                    $workflow = $env->applyUpstreamUpdates(
-                        isset($options['updatedb']) ? $options['updatedb'] : false,
-                        isset($options['accept-upstream']) ? $options['accept-upstream'] : false
-                    );
-                    while (!$workflow->checkProgress()) {
-                        // @TODO: Add Symfony progress bar to indicate that something is happening.
-                    }
-                    $this->log()->notice($workflow->getMessage());
+    /**
+     * @param $options
+     * @param $site
+     * @param $updates
+     * @param $env
+     */
+    protected function applyUpdates($options, $site, $updates, $env)
+    {
+        $logname = $options['dry-run'] ? 'DRY RUN' : 'notice';
+        $this->log()->notice(
+            'Applying {updates} updates to {site}',
+            ['site' => $site->getName(), 'updates' => count($updates), 'name' => $logname]);
+
+        // Do the actual updates if we're not in dry-run mode
+        if (!$options['dry-run']) {
+            // @TODO: We may be able to run workflows asynchronously to save time.
+            try {
+                $workflow = $env->applyUpstreamUpdates(
+                    isset($options['updatedb']) ? $options['updatedb'] : false,
+                    isset($options['accept-upstream']) ? $options['accept-upstream'] : false
+                );
+                while (!$workflow->checkProgress()) {
+                    // @TODO: Add Symfony progress bar to indicate that something is happening.
+                }
+                $this->log()->notice($workflow->getMessage());
+            }
+            catch(\Exception $e) {
+                if (!$options['continue-on-failure']) {
+                    throw new TerminusException($e->getMessage());
+                }
+                else {
+                    $this->log()->warning($e->getMessage());
                 }
             }
         }
